6759f90c5be8ca386f54836e0abdd2926ce14785
Alex Tarrago <alextarrago@gmail.com>
2013-07-08 12:10:19 +0000

    Modified web.js

--- web.js
+++ web.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var fs = require('fs');
-var buf = new Buffer('test');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buf.toString(fs.readFileSync('index.html')));
+ var buf = new Buffer(fs.readFileSync('index.html')); 
+ response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
