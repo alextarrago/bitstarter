3efdc2c9eb94605da3517bab5a935b9eb9a62b4d
Alex Tarrago <alextarrago@gmail.com>
2013-07-08 12:02:39 +0000

    Modified web.js

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(fs.ReadFileSync('/bitstarter/index.html'));
 });
 
 var port = process.env.PORT || 5000;
